f27f2fceb3047d84301af036bd27fd6c729a2508
Marco Crespo <marcoc8@Marcos-MacBook-Air.local>
2022-11-17 17:08:44 -0500

    lab complete

--- routes/characters.routes.js
+++ routes/characters.routes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const axios = require("axios");
 
+const Character = require('../models/character.model');
+
 /* GET home page */
 router.get("/characters", (req, res, next) => {
     axios.get("https://ih-crud-api.herokuapp.com/characters")
@@ -19,6 +21,50 @@ router.get("/characters/:id", (req, res, next) => {
         res.render("characters/details-character", { character: responseFromAPI.data });
     })
     .catch(err => console.error(err))
+});
+
+router.get("characters/create", (req, res, next) => {
+    res.render('character/create-character.hbs')
+});
+
+router.post("/characters/create", (req, res, next) => {
+    const createCharacter = {
+        name: req.body.name,
+        occupation: req.body.occupation,
+        weapon: req.body.weapon,
+        debt: req.body.debt
+    };
+    axios.post(`https://ih-crud-api.herokuapp.com/characters`)
+    .then(responseFromAPI => {
+        Character.create({responseFromAPI})
+        res.redirect('/characters')
+    })
+    .catch(err => console.log(err));
+})
+
+router.post('/characters/:id/update', (req, res, next) => {
+
+    const createCharacter = { 
+        name: req.body.name,
+        occupation: req.body.occupation,
+        weapon: req.body.weapon,
+        debt: req.body.debt
+ };
+
+    axios.put(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`, createCharacter)
+    .then(responseFromAPI => {
+
+        res.redirect(`/characters/${req.params.id}`)
+    })
+    .catch(err => console.log(err));
+});
+
+router.post('/characters/:id/delete', (req, res, next) => {
+    axios.delete(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
+    .then(responseFromAPI => {
+        res.redirect('/characters')
+    })
+    .catch(err => console.log(err));
 });
 
 module.exports = router;
